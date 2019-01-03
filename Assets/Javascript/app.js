05344e2b611037ecee1621e4de5b55d5dc3504d9
Elicas5931 <elizabeth.perry.339@gmail.com>
2019-01-03 10:28:56 -0500

    All the current files I was working on were on another computer

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,233 +1,157 @@
-
-// Create a trivia timed game! Lol
-// Create variables needed for game. May need to add more to HTML for this to work 
-//All the questions we will ask 
-// Questions user got correct
-// Questions user got wrong
-// Questions user did not answer due to time
-// Store user's answer
-// Set a timer for 10 seconds at the top that will count down to the user
-// User can click start button to see first question and then begin timer
-// Have a set of 10 questions that shuffle through in the <div>
-// Only show 1 question and it's 4 answers on the page at a time for 30 seconds
-// Start timer when new question comes up
-// If user gets answer right, display they got it right with a text of correct answer and gif/picture 5 seconds?
-// If user gets it wrong, say it was wrong and still show text of correct answer and gif/picture for 5 seconds?
-// If user does not answer in given time, show it's out of time with the correct answer and move to next questions.
-// Go through all 10 questions
-// Keep score of right and wrong choices as well as unanswered ones 
-// Display score at the end of the game
-// Add a "start over button" that does NOT refresh the page, but just resets the game
-
 $(document).ready(function () {
+    $("#resetTrivia").hide();
+    
+    var questionCounter = 0;
+    
+    //Shows user correct answer
+    var showTimer;
+    
+    //Timer to choose an answer
+    var answerTime = 20;
+    var answerTimer;
 
-// Questions user will answer
-var myQuestions = [{
-    question: "Which fighting game has sold the most copies in history?",
-    choices: ["Super Smash Bros Brawl", "Mortal Kombat X", "Tekken 5", "Street Fighter 2"],
-    actAnswer: 0
-},
-{
-    question: "Which is the highest grossing video game franchise?",
-    choices: ["Grand Theft Auto", "Mario", "Pokemon", "Call of Duty"],
-    actAnswer: 1
-},
-{
-    question: "What is believed to be the first ever MMORPG?",
-    choices: ["Ultima Online", "Everquest", "World of Warcraft", "Meridian 59"],
-    actAnswer: 3
-},
-{
-    question: "You have found yourself in the land of Val Oriyn.... what game are you playing?",
-    choices: ["World of Warcraft", "Tera", "Skyrim", "Diablo 3"],
-    actAnswer: 1
-},
-{
-    question: "What is the highest selling console of all time?",
-    choices: ["Sony's PlayStation 2", "Microsoft's Xbox 360", "Nintendo's DS", "Sega Genesis"],
-    actAnswer: 0
-},
-{
-    question: "Which of these is the best selling game of all time on Xbox One?",
-    choices: ["Destiny 2", "Grand Theft Auto 4", "Call of Duty: WWII", "Battlefield 4"],
-    actAnswer: 2
-},
-{
-    question: "Which of these video game characters never speaks?",
-    choices: ["Master Chief", "Mario", "Nathan Drake", "Link"],
-    actAnswer: 3
-},
-{
-    question: "Which was the first ever video game console?",
-    choices: ["Magnavox Odyssey", "Atari 2600", "Intellivision", "Sega SG-100"],
-    actAnswer: 0
-},
-{
-    quesiton: "What is the maximum number of supported controllers for the Playstation 3?",
-    choices: ["Four", "Six", "Seven", "Ten"],
-    actAnswer: 2
-},
-{
-    question: "What is Megaman's name in Japan?",
-    choices: ["Misterman", "Amazingman", "Rockman", "Superman"],
-    actAnswer: 2
-}
-];
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
 
-var questionCounter = 0;
+    var userAnswer = [];
 
-var showAnswers = 5000;
+    var myQuestions = [
+        "Which fighting game has sold the most copies in history?",
+        "What is believed to be the first ever MMORPG?",
+        "You have found yourself in the land of Val Oriyn.... what game are you playing?",
+        "What is the highest selling console of all time?",
+        "Which of these is the best selling game of all time on Xbox One?",
+        "Which of these video game characters never speaks?",
+        "Which was the first ever video game console?",
+        "What is the maximum number of supported controllers for the Playstation 3?",
+        "What is Megaman's name in Japan?"
+    ];
+    var myAnswers = [
+        ["Super Smash Bros Brawl", "Mortal Kombat X", "Tekken 5", "Street Fighter 2"],
+        ["Ultima Online", "Everquest", "World of Warcraft", "Meridian 59"],
+        ["World of Warcraft", "Tera", "Skyrim", "Diablo 3"],
+        ["Sony Playstation 2", "Microsoft Xbox 360", "Nintendo DS", "Sega Genesis"],
+        ["Destiny 2", "Grand Theft Auto 4", "Call of Duty: WWII", "Battlefield 4"],
+        ["Master Chief", "Mario", "Nathan Drake", "Link"],
+        ["Magnavox Odyssey", "Atari 2600", "Intellivision", "Sega SG-100"],
+        ["Four", "Six", "Seven", "Ten"],
+        ["Misterman", "Amazingman", "Rockman", "Superman"],
+    ];
+    var actAnswer = [
+        "Super Smash Bros Brawl",
+        "Meridian 59",
+        "Tera",
+        "Sony PlayStation 2",
+        "Call of Duty: WWII",
+        "Link",
+        "Magnavox Odyssey",
+        "Seven",
+        "Rockman"
+    ];
+    var selectedAnswer;
+    var gameTrivia; //same as gameHTML
 
-// User score
-var correct = 0;
-var wrong = 0;
-var unanswered = 0;
+    function startPage() {
+        $("#startButton").click(function (click) {
+            $("#beginning").hide();
+            $("#startUp").show();
+            displayQuestions();
+            showChoices();
+            answerTimer();
+        })
+    }
 
-var userAnswer = [];
+    startPage();
 
-var actAnswer
-
-var time = 21
-var timer;
-
-var answerTime = 20;
-
-var answerTimer;
-
-var startTrivia;
-
-alert("Welcome to trivial trivia!");
-
-
-
-    $("#startUp").hide();
-
-    // either do it the following way or change the button in html...
-    // $("#startButton").onClick = function() {start};
-    $("#startButton").click(function () {
-        $("p").hide();
-        $("h1").hide();
-        $("#startButton").hide();
-        $("#startUp").show();
-        startTrivia();
+    $("#answerChoices").on("click", ".answer", function(event){
+        selectedAnswer = $(this).text();
+        if (selectedAnswer === actAnswer[questionCounter]) {
+            clearInterval(answerTimer);
+            addWin();
+        } else {
+            clearInterval(answerTimer);
+            addLoss();
+        }
     });
 
+    $("#resetGame").on("click", "#resetTrivia", function(event) {
+        resetGame();
+    });
 
-function submitAnswer() {
-    $("").on("click", )
-}
+    function displayQuestions() {
+        $("#triviaQuestions").html(myQuestions[questionCounter]); 
+    }
 
-    var startTrivia = function () {
-
-        $(".buttons").empty();
-
-        for (var i = 0; i < 10; i++) {
-
-            var questionCount = $(myQuestions).length;
-
-            questionOrder = [];
-
-            while (questionOrder.length < questionCount) {
-                var random = Math.floor(Math.random() * questionCount);
-                if (questionOrder.indexOf(random) === -1) questionOrder.push(random);
-                questionAsked = 0;
-                hideAllEx(questionOrder[questionAsked]);
+    function answerTimer() {
+        answerTimer = setInterval(twentySeconds, 1000);
+        function twentySeconds() {
+            if (answerTime === 0) {
+                clearInterval(answerTimer);
+                answerTimeout();
             }
-
-            for (var i = 0; i < 4; i++) {
-            var options = $("<button>");
-            options.attr({
-                "id": 'button' + i,
-            });
-            $(".buttons").append(options);
+            if (answerTimer > 0) {
+                answerTimer--;
+            }
+            $("#countdown").html(answerTimer);    
         }
     }
 
+    function showChoices() {
+        triviaChoices = "<p class='answer'>1. " + myAnswers[questionCounter][0] + "</p><p class='answer'>2. " + myAnswers[questionCounter][1] + "</p><p class='answer'>3. " + myAnswers[questionCounter][2] + "</p><p class='answer'>4. " + myAnswers[questionCounter][3] + "</p>";
+        $("#answerChoices").html(triviaChoices);
+        console.log(triviaChoices);
     }
 
-    function hideAllEx(question) {
-        for (var i = 0; i < myQuestions.length; i++) {
-            var question = myQuestions[i].question;
-            console.log(question);
-            var choices = myQuestions[i].choices;
-            console.log(choices);
-        }
+    function addWin() {
+        correct++;
+        $("#quizSection").html(gameTrivia);
+        gameTrivia = "<h3>You guessed correctly! The answer is " + actAnswer[questionCounter] + "!</h3>";
+    }
 
+    function addLoss() {
+        incorrect++;
+        $("#quizSection").html(gameTrivia);
+        gameTrivia = "<h3>You guessed incorrectly! The answer is " + actAnswer[questionCounter] + "!</h3>";
+    }
+
+    function answerTimeout() {
+        unanswered++;
+        gameTrivia = "<p>Time is up!</p><p>The correct answer was: " + actAnswer[questionCounter] + "</p>";
+        $("#quizSection").html(gameTrivia);
+        setTimeout(wait, 5000);
+    }
+
+    function lastScreen() {
+        $("#quizSection").html(gameTrivia);
+        gameTrivia = "<p>You've finished the game! Here's your score</p>" + "<p>Correct Answers: " + correct + "</p>" + "<p>Wrong Answers: " + incorrect + "</p>" + "<p>Unanswered: " + unanswered + "</p>";
+        $("#resetTrivia").show();
+    }
+
+    function resetGame () {
+        questionCounter = 0;
+        correct = 0;
+        incorrect = 0;
+        unanswered = 0;
+        answerTime = 20;
+        displayQuestions();
+        answerTimer();
     }
 
 
 
 
-    function run() {
-        clearInterval(timer);
-        timer = setInterval(decrease, 1000);
-    }
-
-    function decrease() {
-        time--;
-        $("#countdown").html("<h3>You have " + time + " seconds left!</h3>");
-        if (time === 0) {
-            stop();
-        }
-    }
-
-    function stop() {
-        clearInterval(timer);
-    }
-
-    run();
-}
-
-
-
-    // Countdown timer to answer question
 
 
 
 
 
-    // Show answers page
-    //function showAnswers() {
-    //  answerTimer = setInterval(function () {
-    //    answerTime--;
-    //  $("#").html("<h2>The correct answer is " + actAnswer + "!</h2>");
-    //}, 1000)
-
-    //if (answerTime === 0) {
-    //  timer();
-    //}
-
-
-    //var score = function () {
-   // if (userAnswer === actAnswer) {
-    //    correct++;
-  //  } else {
-   //     wrong++;
-   // }
-
-//}
-
-
-//})
-
-
-// function myQuestions(question, choices, actAnswer) {
-    //this.question = question;
-    //this.choices = choices;
-    //this.actAnswer = actAnswer;
-//}
-
-//function setupTrivia() {
-  //$('#triviaQuestions').html(parseInt(currentQuestion) + 1 + " . " + myQuestions[currentQuestion].question);
-   //console.log(currentQuestion);
-//}
 
 
 
 
 
-//var start = function() {
+    // Run start page after reset button
+    startPage();
 
-  //  function questions(text, choices, answer) {
-
-    // 
+})
